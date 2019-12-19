590e71b6d48904e279f25187bd4b3e7266edc1e1
Matt Chapman <matt@mttchpmn.com>
2019-12-20 11:22:40 +1300

    Reconfigure for Heroku

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const scrapeIfis = require("./scrapeIfis");
 
 const app = express();
-const port = 4500;
+const port = process.env.PORT || 8000;
 
 app.get("/", (req, res) => res.send(`API listening on port ${port}`));
 
